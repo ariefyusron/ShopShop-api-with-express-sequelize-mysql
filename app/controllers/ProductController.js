a28b2a60e76d9b6d94c9aaa29ab8c62d0ce7f796
Arief Yusron <ariefyusron98@gmail.com>
2019-01-02 03:09:54 +0700

    validate in model product

--- app/controllers/ProductController.js
+++ app/controllers/ProductController.js
@@ -12,5 +12,10 @@ exports.store = (req,res,next) => {
   models.Products.create(req.body)
     .then((results) => {
       res.json(results)
-    }).catch(next)
+    }).catch((next) => {
+      res.status(500).json({
+        field: next.errors[0].path,
+        message: next.errors[0].message
+      })
+    })
 }
--- models/product.js
+++ models/product.js
@@ -1,9 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Products = sequelize.define('Products', {
-    name: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    image_url: DataTypes.TEXT
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: true
+      }
+    },
+    image_url: {
+      type: DataTypes.TEXT,
+      validate: {
+        isUrl: true
+      }
+    }
   }, {});
   Products.associate = function(models) {
     // associations can be defined here

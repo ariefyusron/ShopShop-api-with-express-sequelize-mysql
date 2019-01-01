a319803347c30c35bbd06e64877a140dfea1d5ff
Arief Yusron <ariefyusron98@gmail.com>
2019-01-02 02:23:31 +0700

    fix ProductController

--- app/controllers/ProductController.js
+++ app/controllers/ProductController.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const models = require('../../models')
 
 exports.index = (req,res) => {
-  res.json('Products')
+  models.Products.findAll()
+    .then((results) => {
+      res.json(results)
+    })
 }
 
-exports.store = (req,res) => {
-  res.json('Store Product')
+exports.store = (req,res,next) => {
+  models.Products.create(req.body)
+    .then((results) => {
+      res.json(results)
+    }).catch(next)
 }
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const router = require('./app/router')
 
 app.get('',(req,res) => {
   res.json('Welcome to API ShopShop')
 })
+app.use(bodyParser.json())
 app.use(router)
 
 app.listen(process.env.PORT,() => {
